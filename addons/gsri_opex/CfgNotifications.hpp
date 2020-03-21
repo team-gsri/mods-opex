e11a9d38d217e7290fab6c75fd387824dff73d47
ImplicitDeny <quentin.aymard21@gmail.com>
2020-03-21 18:40:19 +0100

    Finally fix the hella notification system

---
+++ addons/gsri_opex/CfgNotifications.hpp
@@ -0,0 +1,59 @@
+class CfgNotifications
+{
+	class SubmarineOk
+	{
+		title = "GSRI Insertion System";
+		iconPicture = "\A3\ui_f\data\map\mapcontrol\taskIconDone_ca.paa";
+		iconText = "";
+		description = $STR_GSRI_FREMM_submarineOk;
+		color[] = {1,1,1,1};
+		duration = 5;
+		priority = 0;
+		difficulty[] = {};
+	};
+	class SubmarineFail
+	{
+		title = "GSRI Insertion System";
+		iconPicture = "\A3\ui_f\data\map\mapcontrol\taskIconFailed_ca.paa";
+		iconText = "";
+		description = $STR_GSRI_FREMM_submarineFail;
+		color[] = {1,1,1,1};
+		duration = 5;
+		priority = 0;
+		difficulty[] = {};
+	};
+	class SubmarineInfo
+	{
+		title = "GSRI Insertion System";
+		iconPicture = "\A3\ui_f\data\map\mapcontrol\quay_ca.paa";
+		iconText = "";
+		description = $STR_GSRI_FREMM_submarineInfo;
+		color[] = {1,1,1,1};
+		duration = 5;
+		priority = 0;
+		difficulty[] = {};
+	};
+	class HeliInfo
+	{
+		title = "GSRI Insertion System";
+		iconPicture = "\A3\ui_f\data\map\vehicleicons\iconhelicopter_ca.paa";
+		iconText = "";
+		description = $STR_GSRI_FREMM_heliInfo;
+		color[] = {1,1,1,1};
+		duration = 5;
+		priority = 0;
+		difficulty[] = {};
+	};
+	class HeliDelete : HeliInfo
+	{
+		description = $STR_GSRI_FREMM_heliDelete;
+	};
+	class HeliMoved : HeliInfo
+	{
+		description = $STR_GSRI_FREMM_heliMoved;
+	};
+	class HeliFRIES : HeliInfo
+	{
+		description = $STR_GSRI_FREMM_heliFRIES;
+	};
+};
--- addons/gsri_opex/config.cpp
+++ addons/gsri_opex/config.cpp
@@ -103,3 +103,5 @@ class CfgFunctions {
 		};
 	};
 };
+
+#include "CfgNotifications.hpp"
